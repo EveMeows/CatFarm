d6f7c6b1293d8871575b4252800182f7769036ea
Evelyn Serra <eve.meows@proton.me>
2025-02-05 19:29:21 +0200

    Clean up shop UI. And add basic overlay.

--- Scripts/Worlds/game.gd
+++ Scripts/Worlds/game.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
 @export var shop: Control
+@export var coin: Label
 
 
 func _ready() -> void:
 	RenderingServer.set_default_clear_color(
 		Color(0.379, 0.579, 0.94)
 	)
+	
+	coin.text = str(Global.coins) + "g"
 
 
 func _process(_delta: float) -> void:
